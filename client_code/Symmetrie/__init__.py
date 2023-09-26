096c262285f7164428f92d7af607c896c7c17485
wimstange53@gmail.com <wimstange53@gmail.com>
2023-09-26 05:34:05 +0000

    Created Symmetrie and symmetrie.png; Edited settings; Deleted Form1

--- client_code/Symmetrie/__init__.py
+++ client_code/Symmetrie/__init__.py
@@ -1,7 +1,7 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import SymmetrieTemplate
 from anvil import *
 
-class Form1(Form1Template):
+class Symmetrie(SymmetrieTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
