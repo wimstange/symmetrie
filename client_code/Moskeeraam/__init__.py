fbf3731cc23d517a4d2a60b46fcac9ff0ae71b1b
wimstange53@gmail.com <wimstange53@gmail.com>
2023-10-02 12:46:21 +0000

    Edited settings and Moskeeraam

--- client_code/Moskeeraam/__init__.py
+++ client_code/Moskeeraam/__init__.py
@@ -19,8 +19,8 @@ class Moskeeraam(MoskeeraamTemplate):
         X, Y = [1,1,4], [0,3,3]
         for I in range(1,5):
             for N in range(1,3):
-                X[3*I+N]=X[N]*math.cos(math.PI*I)-Y[N]*math.sin(math.PI*I)
-                Y[3*I+N]=X[N]*math.sin(math.PI*I)+Y[N]*math.cos(math.PI*I)
+                X[3*I+N]=X[N]*math.cos(math.pi*I)-Y[N]*math.sin(math.pi*I)
+                Y[3*I+N]=X[N]*math.sin(math.pi*I)+Y[N]*math.cos(math.pi*I)
 
         for I in range(0,5):
             for N in range(1,3):
