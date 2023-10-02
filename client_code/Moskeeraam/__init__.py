4928839abc24423a02a7fc526236a21e466cc503
wimstange53@gmail.com <wimstange53@gmail.com>
2023-10-02 21:30:47 +0000

    Edited settings and Moskeeraam

--- client_code/Moskeeraam/__init__.py
+++ client_code/Moskeeraam/__init__.py
@@ -16,7 +16,8 @@ class Moskeeraam(MoskeeraamTemplate):
         c.begin_path()
         c.stroke_style = "rgba(0,0,0,1)"    # ""#2196F3"
         c.line_width = 1
-        X, Y = [1,1,4], [0,3,3]
+        X = [0,1,1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        Y = [0,0,3,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
         for I in range(1,5):
             for N in range(1,3):
                 X[3*I+N]=X[N]*math.cos(math.pi*I)-Y[N]*math.sin(math.pi*I)
@@ -24,8 +25,8 @@ class Moskeeraam(MoskeeraamTemplate):
 
         for I in range(0,5):
             for N in range(1,3):
-                X[3*(6+i)+N]=X[3*I+N]
-                Y[3*(6+i)+N]=-Y[3*I+N]
+                X[3*(6+I)+N]=X[3*I+N]
+                Y[3*(6+I)+N]=-Y[3*I+N]
         for N2 in range(6):
             if N2 % 2 == 0:
                 B = 0
@@ -35,9 +36,12 @@ class Moskeeraam(MoskeeraamTemplate):
             for J in range(11):
                 X1 = 12+4*B+8*N1
                 Y1 = 8+6.93*N2
-                c.move_to(X1+X[3*J]+1,Y1+Y[3*J]+K)
+                c.move_to(25+25*(X1+X[3*J+1]),25+25*(Y1+Y[3*J+1]))
                 for K in range(2,3):
-                    c.line_to((X1+X[3*J+K],Y1+Y[3*J+K]))
+                    c.line_to((25+25*(X1+X[3*J+K]),25+25*(Y1+Y[3*J+K])))
+                    c.stroke()
+        print(X)
+        print(Y)
 
     def canvas_1_reset(self, **event_args):
         """This method is called when the canvas is reset and cleared, such as when the window resizes, or the canvas is added to a form."""
