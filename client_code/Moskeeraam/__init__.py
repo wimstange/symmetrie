70e7f9e1a8fc6be169f5386055256354d2514ac1
wimstange53@gmail.com <wimstange53@gmail.com>
2023-10-02 09:01:15 +0000

    Edited Moskeeraam

--- client_code/Moskeeraam/__init__.py
+++ client_code/Moskeeraam/__init__.py
@@ -8,3 +8,14 @@ class Moskeeraam(MoskeeraamTemplate):
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
+
+    def teken_moskeeraam(self):
+        c = self.canvas_1
+        c.clear_rect(0,0,self.canvas_1.get_width(),self.canvas_1.get_height()) 
+        c.begin_path()
+        c.stroke_style = "rgba(0,0,0,1)"    # ""#2196F3"
+        c.line_width = 1
+        X, Y = [1,1,4], [0,3,3]
+        for I in range(1,5):
+            for N in range(1,3):
+                
