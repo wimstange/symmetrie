973aa1f2f2fccd968ad7fdeb6b9d958579074cc7
wimstange53@gmail.com <wimstange53@gmail.com>
2023-10-10 10:28:06 +0000

    Edited Trip

--- client_code/Trip/__init__.py
+++ client_code/Trip/__init__.py
@@ -56,9 +56,9 @@ class Trip(TripTemplate):
                     for K in range(2,M1+1):
                         P, Q = 150+10*(X1+X[J][K]),150+10*(Y1+Y[J][K])
                         c.line_to(P,Q)
-                        time.sleep(.5)
+                    
                         c.stroke()
-                    time.sleep(1)
+                
                         
 
     
