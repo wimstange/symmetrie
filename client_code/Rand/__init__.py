fe44fecc0c89d945f59e3b7eac1fb4e58c814dd3
wimstange53@gmail.com <wimstange53@gmail.com>
2023-10-12 18:13:34 +0000

    Edited Rand

--- client_code/Rand/__init__.py
+++ client_code/Rand/__init__.py
@@ -9,7 +9,7 @@ class Rand(RandTemplate):
         self.init_components(**properties)
         # Any code you write here will run before the form opens.
         self.M = 11
-        self.C = 10               # horizontale verplaatsing van het moetief
+        self.C = int(self.text_box_1.text   )            # horizontale verplaatsing van het moetief
         self.C0 = self.C
         self.H, self.V, self.P, self.Q = 120, 90, 1, 1
         self.N = int((self.H-16)/self.C)
@@ -17,7 +17,7 @@ class Rand(RandTemplate):
         self.Y = [0, -8, 8, 8, 2, 2, 4, 4, 6, 6, 0, 0]
         
     def teken_randversieringen(self):
-
+        self.C = int(self.text_box_1.text   )            # horizontale verplaatsing van het moetief
         c = self.canvas_1
         c.clear_rect(0,0,self.canvas_1.get_width(),self.canvas_1.get_height()) 
         c.begin_path()
@@ -46,12 +46,48 @@ class Rand(RandTemplate):
                 
             print("Keuze TV")
         if self.type.selected_value == "TC":
+            self.C0 = 2*self.C
+            for I in range(1,self.N+1):
+                self.teken_stuk(I)
+                self.teken_stuk(I)
+                self.P = -self.P
+                self.Q = -self.Q
+                self.teken_stuk(I)
+                
             print("Keuze TC")
         if self.type.selected_value == "THV":
+            self.C0 = 2*self.C
+            for I in range(1,self.N+1):
+                self.teken_stuk(I)
+                self.P = -self.P
+                self.teken_stuk(I)
+                self.Q = -self.Q
+                self.teken_stuk(I)
+                self.P = -self.P
+                self.teken_stuk(I)
+                
             print("Keuze THV")
         if self.type.selected_value == "G":
+            self.C0 = self.C
+            for I in range(1, 2*self.N+1, 2):
+                self.teken_stuk(I)
+            self.Q = -self.Q
+            for I in range(1, 2*self.N+1, 2):
+                self.teken_stuk(I)
+                
             print("Keuze G")
         if self.type.selected_value == "GV":
+            self.C0 = 2*self.C
+            for I in range(1, self.N+1, 2):
+                self.teken_stuk(I)
+                self.P = -self.P
+                self.teken_stuk(I)
+            self.Q = -self.Q
+            for I in range(2, self.N+1, 2):
+                self.teken_stuk(I)
+                self.P = -self.P
+                self.teken_stuk(I)
+ 
             print("Keuze GV")
 
  
@@ -80,6 +116,13 @@ class Rand(RandTemplate):
         print("type verandert")
         print(self.type.selected_value)
         self.teken_randversieringen()
+
+    def text_box_1_pressed_enter(self, **event_args):
+        """This method is called when the user presses Enter in this text box"""
+        print("Translatieafstand verandert")
+        print(self.text_box_1.text)
+        self.teken_randversieringen()
+
 
 
             
